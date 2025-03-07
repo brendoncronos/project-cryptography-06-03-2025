a9e22acb2eae20da16f5359bfaa44035935cfbb5
brendoncronos <brendongomes39@gmail.com>
2025-03-07 00:49:22 +0000

    update hashing.js

--- hashing.js
+++ hashing.js
@@ -6,9 +6,7 @@ const senha = "minha_senha_super_secreta";
 // Hash usando SHA-256 (fixo)
 const sha256Hash = crypto
     .createHash('sha256')
-    .update(senha)
+    .update(senha+"chinforinfula")
     .digest('hex'); // ou 'hex', 'base64', 'ascii', 'binary'
 
 console.log(`SHA-256 Hash com ${sha256Hash.length} caracteres: ${sha256Hash}`);
-
-// Hash usando bcrypt (com salt)
